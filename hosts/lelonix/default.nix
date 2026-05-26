ed850da6e4c87e511e504db9249d82aa68982c83
hilorioze <me@hilorioze.com>
2026-05-26 15:21:45 +0100

    fix({hilonix,lelonix}): drop stale btmtk kernel patch

--- hosts/lelonix/default.nix
+++ hosts/lelonix/default.nix
@@ -22,19 +22,6 @@
   boot = {
     binfmt.emulatedSystems = ["aarch64-linux"];
 
-    kernelPatches = [
-      # https://github.com/NixOS/nixpkgs/issues/521528
-      {
-        name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
-
-        patch = pkgs.fetchurl {
-          url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
-
-          hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
-        };
-      }
-    ];
-
     loader.efi = {
       efiSysMountPoint = "/efi";
 
