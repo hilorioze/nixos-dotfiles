a2c110a1d08e874d2bb1a9f1ab1221f258b4e0b6
hilorioze <me@hilorioze.com>
2026-05-31 20:58:34 +0200

    chore(containers/torrserver): update image to `MatriX.141.4`

--- hosts/common/features/containers/torrserver.nix
+++ hosts/common/features/containers/torrserver.nix
@@ -2,7 +2,7 @@
   networking.firewall.allowedTCPPorts = [8090];
 
   virtualisation.oci-containers.containers.torrserver = {
-    image = "ghcr.io/yourok/torrserver@sha256:afed5d6ff406e6a32bad627a225d8c36d344d48f23df9c29c9b6d5324cd491e6"; # MatriX.141
+    image = "ghcr.io/yourok/torrserver@sha256:a06f7edeae97e4c84ee2c3a78c088ffccbb59ea1323ee02d4e158b047896fdbc"; # MatriX.141.4
 
     # for DHT peer discovery
     extraOptions = ["--network=host"];
