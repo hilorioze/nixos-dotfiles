9c25430aff871aae6581eb4816f765ee1f2d64d0
hilorioze <me@hilorioze.com>
2026-06-03 18:27:24 +0200

    fix(hosts): enable `dconf` on headless servers to fix `ca.desrt.dconf` error

--- hosts/fakesynology-nixos/features/default.nix
+++ hosts/fakesynology-nixos/features/default.nix
@@ -3,6 +3,7 @@
     # keep-sorted start
     ../../common/features/avahi.nix
     ../../common/features/cloudflare-ddns.nix
+    ../../common/features/dconf.nix # fixes `ca.desrt.dconf` error (https://github.com/nix-community/home-manager/blob/f384af1bec6423a0d4ba1855917ab948f64e5808/docs/manual/faq/ca-desrt-dconf.md)
     ../../common/features/fluent-bit.nix
     ../../common/features/home-manager.nix
     ../../common/features/node-exporter.nix
--- hosts/hel0/features/default.nix
+++ hosts/hel0/features/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     # keep-sorted start
+    ../../common/features/dconf.nix # fixes `ca.desrt.dconf` error (https://github.com/nix-community/home-manager/blob/f384af1bec6423a0d4ba1855917ab948f64e5808/docs/manual/faq/ca-desrt-dconf.md)
     ../../common/features/fail2ban.nix
     ../../common/features/fluent-bit.nix
     ../../common/features/home-manager.nix
