5892e98944d86b8270bd07e85b73e8d7c84549de
hilorioze <me@hilorioze.com>
2026-06-17 10:48:17 +0100

    feat({hilonix,lelonix}/i18n): add GB and Ukrainian locales

--- hosts/hilonix/features/i18n.nix
+++ hosts/hilonix/features/i18n.nix
@@ -2,6 +2,11 @@
   i18n = {
     defaultLocale = "en_IE.UTF-8";
 
-    extraLocaleSettings.LANGUAGE = "en_IE:en_GB:en";
+    extraLocales = [
+      "en_GB.UTF-8/UTF-8"
+      "uk_UA.UTF-8/UTF-8"
+    ];
+
+    extraLocaleSettings.LANGUAGE = "en_IE:en_GB:en_US:en";
   };
 }
