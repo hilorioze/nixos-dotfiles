272f0b963baad10615119b4b64e4a0c779b1fc65
hilorioze <me@hilorioze.com>
2026-06-17 13:16:14 +0100

    feat(hel0/mailserver): forward `root` and `postmaster` mail to `me`

--- hosts/hel0/features/mailserver.nix
+++ hosts/hel0/features/mailserver.nix
@@ -4,7 +4,24 @@
 in {
   imports = [../../common/features/mailserver.nix];
 
-  mailserver.x509.useACMEHost = mailHost;
+  mailserver = {
+    # `postmaster@${domain}` aliases to `root@${domain}` in LDAP, but postfix does not re-expand it through the `root@${domain}` forward list
+    forwards = let
+      rootFanout = [
+        # keep-sorted start
+        "me@${domain}"
+        "root@${domain}"
+        # keep-sorted end
+      ];
+    in {
+      # keep-sorted start
+      "postmaster@${domain}" = rootFanout;
+      "root@${domain}" = rootFanout;
+      # keep-sorted end
+    };
+
+    x509.useACMEHost = mailHost;
+  };
 
   sops.secrets."services/rspamd/dkim/${domain}/private-key" = {
     owner = config.services.rspamd.user;
