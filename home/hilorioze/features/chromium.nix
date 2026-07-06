50eb0341349d6e1fa04e6cbc6370d261dd53c42a
hilorioze <me@hilorioze.com>
2026-07-07 00:59:26 +0100

    feat(hilorioze/chromium): add `synclify` extension

--- home/hilorioze/features/chromium.nix
+++ home/hilorioze/features/chromium.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
   imports = [../../common/features/chromium.nix];
 
-  programs.chromium.package = pkgs.ungoogled-chromium;
+  programs.chromium = {
+    package = pkgs.ungoogled-chromium;
+
+    # Synclify
+    extensions = [{id = "okdfcljlaacbdacenfeaiekllplonlfm";}];
+  };
 }
