463ec7f204fbc9539235551f2c5eb4c7e55da843
hilorioze <me@hilorioze.com>
2026-07-15 19:30:56 +0100

    chore(common/nix): drop redundant `nixPath` override

--- hosts/common/features/nix.nix
+++ hosts/common/features/nix.nix
@@ -1,28 +1,23 @@
-{inputs, ...}: let
-  inherit ((import ../../../flake.nix)) nixConfig;
-in {
-  nix = {
-    # pin system channels to flake inputs
-    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-
-    settings =
-      {
-        # keep-sorted start block=yes newline_separated=yes
-        experimental-features = [
-          # keep-sorted start
-          "flakes"
-          "nix-command"
-          # keep-sorted end
-        ];
-
-        trusted-users = [
-          # keep-sorted start
-          "@wheel"
-          "deployer"
-          # keep-sorted end
-        ];
+{
+  nix.settings = let
+    inherit ((import ../../../flake.nix)) nixConfig;
+  in
+    {
+      # keep-sorted start block=yes newline_separated=yes
+      experimental-features = [
+        # keep-sorted start
+        "flakes"
+        "nix-command"
         # keep-sorted end
-      }
-      // nixConfig;
-  };
+      ];
+
+      trusted-users = [
+        # keep-sorted start
+        "@wheel"
+        "deployer"
+        # keep-sorted end
+      ];
+      # keep-sorted end
+    }
+    // nixConfig;
 }
