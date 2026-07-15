60f6e638b0cabc9f91dd79533829d2a4d0369f3a
hilorioze <me@hilorioze.com>
2026-07-15 20:46:23 +0100

    feat(common/hilorioze): authorize `hardware` and `zikkkix` SSH keys

--- hosts/common/users/hilorioze.nix
+++ hosts/common/users/hilorioze.nix
@@ -22,7 +22,9 @@
     openssh.authorizedKeys.keys = [
       # keep-sorted start
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBgdezqxraUd2gGiPbRygTu8LOgiDAIRrUCe61eAO0fV openpgp:0x6B478424"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbfxu8CyP26944VLHYf6tF/aicquTEO0pJ18puivZ1d openpgp:0xE82274DB"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+PP6MKrJ6i4aoUhBAQr4sx3ituSLQnlbfO4+nT99G6 openpgp:0x04FB7E2B"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLmFD0F8wiummNyIlj9fHy52JlwMHYISo+GroWSC2tY openpgp:0xF9C7737F"
       # keep-sorted end
     ];
 
