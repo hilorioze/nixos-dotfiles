621a9bb853216e4446fe92f3f7ca468a27c7fb52
hilorioze <me@hilorioze.com>
2026-07-21 00:43:04 +0100

    fix(hilorioze/vscodium): completely disable hardware acceleration to avoid AMD GPU resets

--- home/hilorioze/hosts/hilonix/features/vscodium.nix
+++ home/hilorioze/hosts/hilonix/features/vscodium.nix
@@ -1,3 +1,7 @@
 {
-  programs.vscodium.profiles.default.userSettings."terminal.integrated.gpuAcceleration" = "off"; # https://github.com/microsoft/vscode/issues/238088
+  home.file.".vscode-oss/argv.json".text = builtins.toJSON {
+    enable-crash-reporter = false; # keep explicit because 'vscodium' otherwise tries to modify this read-only file managed by 'home-manager' and complains that `argv.json` contains errors
+
+    disable-hardware-acceleration = true; # https://github.com/microsoft/vscode/issues/238088
+  };
 }
