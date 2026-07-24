43279f247d2b2609fef20a9e34b3c172fabfc8f4
hilorioze <me@hilorioze.com>
2026-07-24 19:38:12 +0100

    feat(hilorioze/ssh): add `cex` host

--- home/hilorioze/features/ssh.nix
+++ home/hilorioze/features/ssh.nix
@@ -18,6 +18,7 @@
       ${mkKnownHostLine outputs.nixosConfigurations.hilonix ../../../hosts/hilonix/ssh_host_ed25519_key.pub}
       ${mkKnownHostLine outputs.nixosConfigurations.zikkkix ../../../hosts/zikkkix/ssh_host_ed25519_key.pub}
 
+      cex.hilorioze.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFgvannOBsGfTS9JC3hTXRDOmZ47kp6WLfTm6puN/IB
       fakesynology.hilorioze.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL48Td3OYp1cNPORvthU7HCERDli4Zfhsuh2NvnvSB8H
       ${mkKnownHostLine outputs.nixosConfigurations.fakesynology-nixos ../../../hosts/fakesynology-nixos/ssh_host_ed25519_key.pub}
       ${mkKnownHostLine outputs.nixosConfigurations.de0 ../../../hosts/de0/ssh_host_ed25519_key.pub}
@@ -34,6 +35,11 @@
       XAuthLocation = lib.getExe pkgs.xauth; # required for X11 forwarding (`ssh -Y`)
     };
 
+    "cex.hilorioze.com" = {
+      User = "cex";
+
+      ForwardAgent = true; # for git
+    };
     "fakesynology.hilorioze.com" = {
       User = "fakesynology";
 
