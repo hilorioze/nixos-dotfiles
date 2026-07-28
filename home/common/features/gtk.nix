a2910178ef5044c651e35ee2d45b615516a45f94
hilorioze <me@hilorioze.com>
2026-07-28 13:06:22 +0100

    fix(home/gtk): restore `gsettings` schemas for `firefox`/`thunderbird` (NixOS/nixpkgs#546204)

--- home/common/features/gtk.nix
+++ home/common/features/gtk.nix
@@ -1,6 +1,12 @@
-let
+{pkgs, ...}: let
   gtk-decoration-layout = "icon:minimize,maximize,close";
 in {
+  # restore `gsettings` schemas missing from the `firefox`/`thunderbird` wrappers (https://github.com/NixOS/nixpkgs/issues/546204)
+  xdg.systemDirs.data = [
+    "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+  ];
+
   gtk = {
     enable = true;
 
