181d25878170d546c82d5d1abcb491c11edb06be
hilorioze <me@hilorioze.com>
2026-07-30 15:36:05 +0100

    fix(hel0/tailscale): start after `headscale` and `traefik`

--- hosts/hel0/features/tailscale.nix
+++ hosts/hel0/features/tailscale.nix
@@ -6,4 +6,20 @@
 
     extraSetFlags = ["--advertise-exit-node"];
   };
+
+  systemd.services.tailscaled = {
+    after = [
+      # keep-sorted start
+      "headscale.service"
+      "traefik.service"
+      # keep-sorted end
+    ];
+
+    wants = [
+      # keep-sorted start
+      "headscale.service"
+      "traefik.service"
+      # keep-sorted end
+    ];
+  };
 }
