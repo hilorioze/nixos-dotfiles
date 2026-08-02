870fc862f282cbb9869ef6e5ac52e0fea72eb9cd
hilorioze <me@hilorioze.com>
2026-08-02 18:11:16 +0100

    feat(hilonix): sync current monitor layout

--- hosts/hilonix/features/sddm.nix
+++ hosts/hilonix/features/sddm.nix
@@ -15,23 +15,21 @@
       # edidIdentifier: python3 -c "import glob,struct;[print(f[16:-5].split('-',1)[1]+':',''.join(chr(((d[8]<<8|d[9])>>i&31)+64)for i in(10,5,0)),*struct.unpack('<HI',d[10:16]),d[16],d[17]+1990,0)for f in glob.glob('/sys/class/drm/card*-*/edid')if(d:=open(f,'rb').read())[16:]]"
       data = [
         {
-          edidIdentifier = "RTK 15165 16843009 43 2020 0";
-
-          transform = "Rotated90";
-        }
-
-        {
           edidIdentifier = "AOC 12802 1502 50 2023 0";
         }
 
         {
-          edidIdentifier = "BNQ 32639 0 38 2020 0";
+          edidIdentifier = "BNQ 32640 16843009 38 2020 0";
+        }
+
+        {
+          edidIdentifier = "MSI 40130 0 5 2024 0";
 
           mode = {
-            width = 1920;
-            height = 1080;
+            width = 2560;
+            height = 1440;
 
-            refreshRate = 120000; # 144Hz fails on this monitor over MST Hub
+            refreshRate = 144001; # higher refresh rates fail with the current MST hub and DP cable
           };
         }
       ];
@@ -46,99 +44,39 @@
             {
               enabled = true;
 
+              outputIndex = 1;
+
+              priority = 2;
+
+              position = {
+                x = 0;
+                y = 360;
+              };
+            }
+
+            {
+              enabled = true;
+
               outputIndex = 0;
+
+              priority = 1;
+
+              position = {
+                x = 1920;
+                y = 0;
+              };
+            }
+
+            {
+              enabled = true;
+
+              outputIndex = 2;
 
               priority = 3;
 
               position = {
-                x = 0;
+                x = 4480;
                 y = 0;
-              };
-            }
-
-            {
-              enabled = true;
-
-              outputIndex = 1;
-
-              priority = 1;
-
-              position = {
-                x = 1080;
-                y = 90;
-              };
-            }
-
-            {
-              enabled = true;
-
-              outputIndex = 2;
-
-              priority = 2;
-
-              position = {
-                x = 3640;
-                y = 450;
-              };
-            }
-          ];
-        }
-
-        {
-          outputs = [
-            {
-              enabled = true;
-
-              outputIndex = 0;
-
-              priority = 2;
-
-              position = {
-                x = 0;
-                y = 0;
-              };
-            }
-
-            {
-              enabled = true;
-
-              outputIndex = 1;
-
-              priority = 1;
-
-              position = {
-                x = 1080;
-                y = 90;
-              };
-            }
-          ];
-        }
-
-        {
-          outputs = [
-            {
-              enabled = true;
-
-              outputIndex = 1;
-
-              priority = 1;
-
-              position = {
-                x = 0;
-                y = 0;
-              };
-            }
-
-            {
-              enabled = true;
-
-              outputIndex = 2;
-
-              priority = 2;
-
-              position = {
-                x = 2560;
-                y = 360;
               };
             }
           ];
