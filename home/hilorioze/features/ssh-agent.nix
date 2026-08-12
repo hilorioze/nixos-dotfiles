261785f7f34df0ba555369a9563358be40304ae8
hilorioze <me@hilorioze.com>
2026-08-12 03:28:01 +0100

    fix(hilorioze/ssh-agent): configure `ksshaskpass`

--- home/hilorioze/features/ssh-agent.nix
+++ home/hilorioze/features/ssh-agent.nix
@@ -1,3 +1,11 @@
 {
+  # keep-sorted start
+  lib,
+  pkgs,
+  # keep-sorted end
+  ...
+}: {
   services.ssh-agent.enable = true;
+
+  systemd.user.services.ssh-agent.Service.Environment = "SSH_ASKPASS=${lib.getExe pkgs.kdePackages.ksshaskpass}";
 }
