1f3b893a307a2fd0ee3d4120d9d4186c43a35693
hilorioze <me@hilorioze.com>
2026-08-14 17:12:03 +0100

    feat(de0/niks3): allow OIDC auth for `hilorioze/steam-voicechat-fix`

--- hosts/de0/features/niks3.nix
+++ hosts/de0/features/niks3.nix
@@ -71,6 +71,7 @@
           # keep-sorted start
           "repo:hilorioze/cstrike-mod:ref:refs/heads/main"
           "repo:hilorioze/nixos-dotfiles:ref:refs/heads/main"
+          "repo:hilorioze/steam-voicechat-fix:ref:refs/heads/main"
           # keep-sorted end
         ];
       };
