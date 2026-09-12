ae5e76f9cf110478d211cb114eb5ef8840948418
hilorioze <hilorioze@hilorioze.com>
2026-09-12 02:33:24 +0100

    chore(overlays/lazyvim-wakatime): explain resolver fix

--- overlays/lazyvim-wakatime/default.nix
+++ overlays/lazyvim-wakatime/default.nix
@@ -1,4 +1,5 @@
 lazyvimOverlay: final: prev: {
+  # fix lazyvim-nix resolving `wakatime/vim-wakatime` to nonexistent `vim_wakatime` instead of `vim-wakatime` due to `resolvePluginName` normalizing `-` to `_`
   lazyvimPluginMappings =
     (lazyvimOverlay final prev).lazyvimPluginMappings
     // {
