26ffd7f68371f7a22c303fbdd4028718bc6ff019
hilorioze <hilorioze@hilorioze.com>
2026-09-13 15:56:44 +0100

    fix(hilorioze/niks3): use the default XDG auth-token path

--- home/hilorioze/features/niks3.nix
+++ home/hilorioze/features/niks3.nix
@@ -5,14 +5,11 @@
   # keep-sorted end
   ...
 }: {
-  sops.secrets."credentials/niks3/servers/hilorioze/api-token" = {};
+  # niks3's default XDG auth-token path instead of `NIKS3_AUTH_TOKEN_FILE`
+  sops.secrets."credentials/niks3/servers/hilorioze/api-token".path = "${config.xdg.configHome}/niks3/auth-token";
 
   home = {
-    sessionVariables = {
-      NIKS3_SERVER_URL = "https://niks3.hilorioze.com";
-
-      NIKS3_AUTH_TOKEN_FILE = config.sops.secrets."credentials/niks3/servers/hilorioze/api-token".path;
-    };
+    sessionVariables.NIKS3_SERVER_URL = "https://niks3.hilorioze.com";
 
     packages = [pkgs.niks3];
   };
