7235c45d63f7e91222599157678be06190e82020
hilorioze <hilorioze@hilorioze.com>
2026-09-16 20:49:51 +0100

    chore(workflows): add ISO release

--- shared.nix
+++ shared.nix
@@ -2,6 +2,7 @@
   ignoredShellcheckRules = [
     # keep-sorted start
     "SC2016" # expressions in single quotes; intentional, referenced have no shell expansion
+    "SC2046" # unquoted command substitution; intentional, referenced output is a single word
     "SC2086" # unquoted variables; referenced are safe
     "SC2162" # `read` without `-r`; intentional too, paths and stuff don't have backslashes
     "SC2231" # unquoted variable in `for` loop glob; see `SC2086`
